5eed7c436b462e42a6e2271283bc8b24ec160019
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-05-21 23:18:15 -0400

    Initial Commit

---
+++ src/test/java/com/eventscheduler/EventScheduler/EventSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventscheduler.EventScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
