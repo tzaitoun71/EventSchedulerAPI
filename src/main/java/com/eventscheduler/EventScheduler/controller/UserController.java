e086fab386b32d2a9a5c7dab27f314392eba9214
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-06-01 23:56:24 -0400

    Added User Controller

---
+++ src/main/java/com/eventscheduler/EventScheduler/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.eventscheduler.EventScheduler.controller;
+
+import com.eventscheduler.EventScheduler.model.User;
+import com.eventscheduler.EventScheduler.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
